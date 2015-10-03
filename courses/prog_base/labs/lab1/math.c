cf14c59399c55b223edee7ffe75eb390232abd29
kostya9 <kostya9@gmail.com>
2015-10-03 15:15:12 +0300

    NAN in math.c

--- courses/prog_base/labs/lab1/math.c
+++ courses/prog_base/labs/lab1/math.c
@@ -1,9 +1,16 @@
 #include <math.h>
 double calc(double x, double y, double z)
 {
-	double a;
-	a = pow(x, y + 1) / pow(x - y, 1 / z);
-	a += 8*y + z / x;
-	a += sqrt(fabs(cos(y) / sin(x) + 1));
-	return a;
+    double a;
+    if(x!=y && x!=0 && x>=y  )
+    {
+        a = pow(x, y + 1) / pow(x - y, 1 / z);
+        a += 8*y + z / x;
+        a += sqrt(fabs(cos(y) / sin(x) + 1));
+    }
+    else
+    {
+        return NAN;
+    }
+    return a;
 }
