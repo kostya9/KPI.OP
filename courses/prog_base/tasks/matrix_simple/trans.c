aba014dfec8cc43819dca406833dc498435d3a3f
kostya9 <kostya9@gmail.com>
2015-10-23 19:48:35 +0300

    transposeSide->transposeMain. One rand() instead of two.

--- courses/prog_base/tasks/matrix_simple/trans.c
+++ courses/prog_base/tasks/matrix_simple/trans.c
@@ -5,7 +5,7 @@ void fillRand(int mat[4][4])
 	for (i = 0; i < 4; i++)
 		for (n = 0; n < 4; n++)
 		{
-			mat[i][n] = pow(-1, rand() % 2) * rand() % 1000;
+			mat[i][n] = 1000 - rand() % 2000;
 		}
 }
 void matCopy(int src[4][4], int dest[4][4])
@@ -17,7 +17,7 @@ void matCopy(int src[4][4], int dest[4][4])
 			dest[i][n] = src[i][n];
 		}
 }
-void rotateCW90(int mat[4][4])		// (n0 % 6 == 2)
+void rotateCW90(int mat[4][4])		
 {
 	int i, n, k;
 	int tempMat[4][4] = { 0 };
@@ -34,16 +34,14 @@ void rotateCW270(int mat[4][4])
 	for (i = 0; i < 3;i++)
 		rotateCW90(mat);
 }
-void transposSide(int mat[4][4])		// (n0 % 2 == 1)
+void transposeMain(int mat[4][4])		
 {
 	int i, n;
 	int tempMat[4][4];
 	matCopy(mat, tempMat);
 	for (i = 0; i < 4; i++)
 		for (n = 0; n < 4; n++)
-			mat[3-n][3-i] = tempMat[i][n];
-
-		
+			mat[n][i] = tempMat[i][n];
 }
 void flipV(int mat[4][4])
 {
