ba1fda0be2c37de515760889c290ee796e2debcf
kostya9 <kostya9@gmail.com>
2015-11-02 16:22:50 +0200

    Console ready.

---
+++ courses/prog_base/tasks/console/main.c
@@ -0,0 +1,46 @@
+#include <windows.h>
+#define RED BACKGROUND_RED
+#define PURPLE BACKGROUND_RED | BACKGROUND_BLUE
+#define YELLOW BACKGROUND_RED | BACKGROUND_GREEN
+void setCursorPosition(int x, int y)
+{
+    COORD coord = {x, y};
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleCursorPosition(hConsole, coord);
+}
+void setConsoleColor(int curColorPos)
+{
+    int color;
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    if(curColorPos==2)
+        color = RED | BACKGROUND_INTENSITY;
+    else if(curColorPos==1)
+        color = PURPLE | BACKGROUND_INTENSITY;
+    else if(curColorPos==0)
+        color = YELLOW | BACKGROUND_INTENSITY;
+    SetConsoleTextAttribute(hConsole, color);
+}
+void consoleFill(int rows, int columns)
+{
+    int i, curRow;
+    for(i = 0, curRow = 0; curRow<rows; i++, curRow = (i/columns))
+    {
+        int left = (i/columns)%2 ? 0 : 1;
+        int curColumn = left ? (columns-1)-i%columns : i%columns;
+        int curColorPos = abs((curColumn - curRow)%3);
+        setCursorPosition(curColumn, curRow);
+        setConsoleColor(curColorPos);
+        printf("*");
+        Sleep(10);
+    }
+    setCursorPosition(0, 0);
+}
+int main()
+{
+    CONSOLE_SCREEN_BUFFER_INFO csbi;
+    int columns, rows;
+    GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &csbi);
+    columns = csbi.srWindow.Right - csbi.srWindow.Left + 1;
+    rows = csbi.srWindow.Bottom - csbi.srWindow.Top + 1;
+    consoleFill(rows, columns);
+}
