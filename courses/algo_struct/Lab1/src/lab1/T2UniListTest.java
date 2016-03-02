9a9b717b6112fefe1d8298a388227ca3099f3d92
kostya9 <kostya9@gmail.com>
2016-03-02 19:48:18 +0200

    algo_struct lab1 task 2

--- courses/algo_struct/Lab1/src/lab1/T2UniListTest.java
+++ courses/algo_struct/Lab1/src/lab1/T2UniListTest.java
@@ -1,24 +1,93 @@
 package lab1;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class T2UniListTest {
 
 	public static void main(String[] args) {
 		T2UniList l = new T2UniList();
-		printDeleteValue(l, 0);
-		printAddValue(l, 6);
-		printAddValue(l, 4);
-		printAddValue(l, 100);
-		printAddValue(l, 99);
-		printAddValue(l, 65);
-		printGetValue(l, 3);
-		printGetValue(l, 100);
-		printAddValue(l, 100, 1000);
-		printDeleteValue(l, 0);
-		printDeleteValue(l, 2);
-		System.out.println(l.getSize());
 		
 	}
-	static void printAddValue(T2UniList l, int value)
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testInsertIntoNonExistantIndex()
+	{
+		T2UniList list = new T2UniList();
+		list.insert(1, 1);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testGetFromNonExistantIndex()
+	{
+		T2UniList list = new T2UniList();
+		list.getValue(0);
+	}
+	
+	@Test(expected=IndexOutOfBoundsException.class)
+	public void testDeleteFromEmptyList()
+	{
+		T2UniList list = new T2UniList();
+		list.remove(0);
+	}
+	
+	@Test
+	public void testAddFirstGetIt()
+	{
+		T2UniList list = new T2UniList();
+		list.insert(0, 1);
+		list.insert(0, 2);
+		int actual = list.getValue(0);
+		int expected = 2;
+		System.out.println("Inserted values 1 and 2 into the last position");
+		System.out.println(list.toString());
+		assertEquals(expected, actual);
+		actual = list.getValue(1);
+		expected = 1;
+		assertEquals(expected, actual);
+		System.out.println("Success\n");
+	}
+	
+	@Test
+	public void testAddLastGetIt()
+	{
+		T2UniList list = new T2UniList();
+		list.insert(list.getSize(), 1);
+		list.insert(list.getSize(), 2);
+		System.out.println("Inserted values 1 and 2 into the first position");
+		System.out.println(list.toString());
+		int actual = list.getValue(1);
+		int expected = 2;
+		assertEquals(expected, actual);
+		actual = list.getValue(0);
+		expected = 1;
+		assertEquals(expected, actual);
+		System.out.println("Success\n");
+	}
+	
+	@Test
+	public void testRemove()
+	{
+		T2UniList list = new T2UniList();
+		list.insert(list.getSize(), 1);
+		list.insert(list.getSize(), 2);
+		list.insert(list.getSize(), 3);
+		list.remove(0);
+		System.out.println("Added values 1, 2 and 3 to the list. Deleted the first value");
+		System.out.println(list.toString());
+		int actual = list.getValue(0);
+		int expected = 2;
+		assertEquals(expected, actual);
+		list.remove(list.getSize() - 1);
+		System.out.println("Deleted the last value");
+		System.out.println(list.toString());
+		actual = list.getValue(list.getSize() - 1);
+		expected = 2;
+		assertEquals(expected, actual);
+		System.out.println("Success\n");
+	}
+	/*static void printAddValue(T2UniList l, int value)
 	{
 		String s = "";
 		try
@@ -91,6 +160,6 @@ public class T2UniListTest {
 			System.out.println(s);
 			System.out.println(l.toString());
 		}
-	}
+	}*/
 
 }
