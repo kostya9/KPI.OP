87ec70f1554a618151f9cbca2aa4a727b911c561
Kostyantyn Sharovarsky <Kostyantyn Sharovarsky>
2016-03-19 20:35:41 +0200

    now counts calls correctly

--- courses/prog_base_2/tasks/callbacks/main.c
+++ courses/prog_base_2/tasks/callbacks/main.c
@@ -3,18 +3,23 @@
 #include <time.h>
 #include <math.h>
 #include <assert.h>
-typedef (*cb)(int);
-void loop(int N, int K, int L, cb overCB, cb underCB);
-void under(int n);
-void over(int n);
+typedef (*cb)(int, int *);
+void loop(int N, int K, int L, cb overCB, int * n_calls_over, cb underCB, int * n_calls_under);
+void under(int n, int * n_calls);
+void over(int n, int * n_calls);
 
 int main(void)
 {
+    int n_calls_over, n_calls_under;
     srand(time(NULL));
-    loop(10, 6, 4, over, under);
+    loop(10, 6, 3, over, &n_calls_over, under, &n_calls_under);
+    puts("Loop Ended");
+    printf("Under : %i\nOver : %i\n", n_calls_under, n_calls_over);
+    puts("Press anything to end...");
+    getch();
     return EXIT_SUCCESS;
 }
-void loop(int N, int K, int L, cb overCB, cb underCB)
+void loop(int N, int K, int L, cb overCB, int * n_calls_over, cb underCB, int * n_calls_under)
 {
     while(!kbhit())
     {
@@ -22,18 +27,16 @@ void loop(int N, int K, int L, cb overCB, cb underCB)
         assert(randomN != N);
         assert(randomN != -N);
         if(abs(randomN) < L)
-            underCB(randomN);
+            underCB(randomN, n_calls_under);
         else if(abs(randomN) > K)
-            overCB(randomN);
+            overCB(randomN, n_calls_over);
     }
 }
-void under(int n)
+void under(int n, int * n_calls)
 {
-    static int n_calls;
-    n_calls++;
+    (*n_calls)++;
 }
-void over(int n)
+void over(int n, int * n_calls)
 {
-    static int n_calls;
-    n_calls++;
+    (*n_calls)++;
 }
