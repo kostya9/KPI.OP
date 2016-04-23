aa81a940aa118deca2e5cefaeb045f4b3c2cce9b
kostya9 <kostya9@gmail.com>
2016-04-23 17:47:28 +0300

    Incorrect Create statement

--- courses/prog_base_2/tasks/database/sql_queries/des_db_create.sql
+++ courses/prog_base_2/tasks/database/sql_queries/des_db_create.sql
@@ -2,8 +2,10 @@ CREATE TABLE "Designers" (
 	`Id`	INTEGER,
 	`Name`	TEXT NOT NULL,
 	`Surname`	TEXT NOT NULL,
-	`averageVertices`	REAL,
+	`AverageVertices`	REAL,
 	`Experience`	INTEGER DEFAULT 0,
-	`HireDate`	INTEGER DEFAULT 0,
+	`Salary`	REAL,
+	`ProjectCount`	INTEGER,
+	`HireDate`	TEXT DEFAULT 0,
 	PRIMARY KEY(Id)
 )
