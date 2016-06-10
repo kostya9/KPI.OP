03c981691da78b32bcc972a04d068b92f81c60de
kostya9 <kostya9@gmail.com>
2016-06-10 23:05:25 +0300

    deleting useless onedrive things

--- courses/prog_base_3/project/opengllearn/MenuOptionButton-DESKTOP-9J8HOEO.cpp
+++
@@ -1,8 +0,0 @@
-#include "MenuOptionButton.h"
-
-MenuOptionButton::MenuOptionButton(Loader * loader, Font * font, glm::fvec3 color, string text) : MenuOption(font, GuiTexture(), color, text)
-{
-	static GLuint id = loader->loadTexture("models\\menu\\button\\button.png");
-	texture = GuiTexture(id, glm::fvec2(), glm::fvec2(0.19f * 2, 0.045f * 3));
-	alpha = 0.8f;
-}
--- courses/prog_base_3/project/opengllearn/MenuOptionSlider-DESKTOP-9J8HOEO.cpp
+++
@@ -1,7 +0,0 @@
-#include "MenuOptionSlider.h"
-
-MenuOptionSlider::MenuOptionSlider(Loader * loader, Font * font, glm::fvec3 color) : MenuOption(font, GuiTexture(), color, string(""))
-{
-	static GLuint id = loader->loadTexture("models\\menu\\slider\\yellow_sliderLeft.png");
-	texture = GuiTexture(id, glm::fvec2(), glm::fvec2(0.1f, 0.3f));
-}
